f495ac6aeb04ff3ba86fb13549d99d711beb9975
smirnova1970 <s.tanja.g@bk.ru>
2024-09-07 13:17:50 +0300

    Update module_7_4.py

--- module_7_4.py
+++ module_7_4.py
@@ -36,3 +36,32 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+Задание:
+
+Создайте новый проект или продолжите работу в текущем проекте.
+Используйте os.walk для обхода каталога, путь к которому указывает переменная directory
+Примените os.path.join для формирования полного пути к файлам.
+Используйте os.path.getmtime и модуль time для получения и отображения времени последнего изменения файла.
+Используйте os.path.getsize для получения размера файла.
+Используйте os.path.dirname для получения родительской директории файла.
+
+import os
+import time
+
+
+def main():
+    directory = '.'
+    for root, dirs, files in os.walk(directory):
+        print(f'>> root = "{root}", dirs = "{dirs}"')
+        for i, file in enumerate(files):
+            print(f'{i + 1}:', end=' ')
+            filepath = os.path.join(root, file)
+            filetime = os.path.getmtime(filepath)
+            formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+            filesize = os.path.getsize(filepath)
+            parent_dir = os.path.dirname(root)
+            print(
+                f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения:'
+                f' {formatted_time}, Родительская директория: {parent_dir}')
+            
