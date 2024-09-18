7a4496fbefa5d85b51fe8904369d8bd2ce26fd99
Татьяна <s.tanja.g@bk.ru>
2024-09-18 12:52:42 +0300

    Add files via upload

---
+++ module_10_4.py
@@ -0,0 +1,89 @@
+"""Метод discuss_guests(self):
+Этот метод имитирует процесс обслуживания гостей.
+Обслуживание должно происходить пока очередь не пустая (метод empty) или хотя бы один стол занят.
+Если за столом есть гость(поток) и гость(поток) закончил приём пищи(поток завершил работу - метод is_alive), то
+вывести строки "<имя гостя за текущим столом> покушал(-а) и ушёл(ушла)" и "Стол номер <номер стола> свободен".
+Так же текущий стол освобождается (table.guest = None).
+Если очередь ещё не пуста (метод empty) и стол один из столов освободился (None), то текущему столу присваивается
+гость взятый из очереди (queue.get()). Далее выводится строка "<имя гостя из очереди> вышел(-ла) из очереди и сел(-а)
+ за стол номер <номер стола>"
+Далее запустить поток этого гостя (start)
+Таким образом мы получаем 3 класса на основе которых имитируется работа кафе:
+Table - стол, хранит информацию о находящемся за ним гостем (Guest).
+Guest - гость, поток, при запуске которого происходит задержка от 3 до 10 секунд.
+Cafe - кафе, в котором есть определённое кол-во столов и происходит имитация прибытия гостей (guest_arrival) и их
+обслуживания (discuss_guests)."""
+
+from threading import Thread
+from random import randint
+from time import sleep
+from queue import Queue
+
+
+class Table:
+    def __init__(self, number):
+        self.number = number
+        self.guest = None
+
+
+class Guest(Thread):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+
+    def run(self):
+        sleep(randint(3, 10))
+
+
+class Cafe:
+    def __init__(self, *tables):
+        self.queue = Queue()
+        self.tables = list(tables)
+
+    def guest_arrival(self, *guests):
+        for guest in guests:
+            for table in self.tables:
+                if table.guest is None:
+                    table.guest = guest
+                    guest.start()
+                    print(f"{guest.name} сел(-а) за стол номер {table.number}")
+                    break
+            else:
+                self.queue.put(guest)
+                print(f"{guest.name} в очереди")
+
+    def discuss_guests(self):
+        while not self.queue.empty() or any(table.guest is not None for table in self.tables):
+            for table in self.tables:
+                if table.guest is not None and not table.guest.is_alive():
+                    print(f"{table.guest.name} покушал(-а) и ушёл(ушла)")
+                    print(f"Стол номер {table.number} свободен")
+                    table.guest = None
+                    if not self.queue.empty():
+                        next_guest = self.queue.get()
+                        table.guest = next_guest
+                        next_guest.start()
+                        print(f"{next_guest.name} вышел(-ла) из очереди и сел(-а) за стол номер {table.number}")
+                    sleep(1)
+
+
+# Создание столов
+tables = [Table(number) for number in range(1, 6)]
+
+# Имена гостей
+guests_names = [
+ 'Maria', 'Oleg', 'Vakhtang', 'Sergey', 'Darya', 'Arman',
+ 'Vitoria', 'Nikita', 'Galina', 'Pavel', 'Ilya', 'Alexandra'
+]
+
+# Создание гостей
+guests = [Guest(name) for name in guests_names]
+
+# Заполнение кафе столами
+cafe = Cafe(*tables)
+
+# Приём гостей
+cafe.guest_arrival(*guests)
+
+# Обслуживание гостей
+cafe.discuss_guests()
