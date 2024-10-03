7d2a965bb85035df938f68496eda4105920d2790
Татьяна <s.tanja.g@bk.ru>
2024-10-03 21:19:42 +0300

    Add files via upload
    Задача "Проверка на выносливость":
    В первую очередь скачайте исходный код, который нужно обложить тестами с GitHub. (Можно скопировать)
    В этом коде сможете обнаружить класс Runner, объекты которого вам будет необходимо протестировать.
    
    Напишите класс RunnerTest, наследуемый от TestCase из модуля unittest. В классе пропишите следующие методы:
    test_walk - метод, в котором создаётся объект класса Runner с произвольным именем. Далее вызовите метод walk у этого объекта 10 раз. После чего методом assertEqual сравните distance этого объекта со значением 50.
    test_run - метод, в котором создаётся объект класса Runner с произвольным именем. Далее вызовите метод run у этого объекта 10 раз. После чего методом assertEqual сравните distance этого объекта со значением 100.
    test_challenge - метод в котором создаются 2 объекта класса Runner с произвольными именами. Далее 10 раз у объектов вызываются методы run и walk соответственно. Т.к. дистанции должны быть разными, используйте метод assertNotEqual, чтобы убедится в неравенстве результатов.
    Запустите кейс RunnerTest. В конечном итоге все 3 теста должны пройти проверку.
    
    Пункты задачи:
    Скачайте исходный код для тестов.
    Создайте класс RunnerTest и соответствующие описанию методы.
    Запустите RunnerTest и убедитесь в правильности результатов.
    Пример результата выполнения программы:
    Вывод на консоль:
    Ran 3 tests in 0.001s OK

---
+++ tests_12_1.py
@@ -0,0 +1,37 @@
+import runner
+import unittest
+
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        """A method in which an object of the Runner class with an arbitrary name is created.
+        Next, call the walk method on this object 10 times.
+        Then use the assert Equal method to compare the distance of this object with the value 50"""
+        ctg = runner.Runner('wolf')
+        for i in range(10):
+            ctg.walk()
+        self.assertEqual(ctg.distance, 50)
+
+    def test_run(self):
+        """ A method in which an object of the Runner class with an arbitrary name is created. Next,
+        call the run method on this object 10 times. Then use the assert Equal method to compare
+        the distance of this object with the value 100 """
+        ctg1 = runner.Runner("Alice")
+        for i in range(10):
+            ctg1.run()
+        self.assertEqual(ctg1.distance, 100)
+
+    def test_challenge(self):
+        """A method in which 2 objects of the Runner class with arbitrary names are created. Next, the run and
+         walk methods are called 10 times for objects, respectively. Since the distances must be different,
+          use the assertNotEqual method to make sure that the results are unequal."""
+        ctg2 = runner.Runner("hare")
+        ctg3 = runner.Runner("kolobok")
+        for i in range(10):
+            ctg2.walk()
+            ctg3.run()
+        self.assertNotEqual(ctg2.distance, ctg3.distance)
+
+
+if __name__ == "__main__":
+    unittest.main()
